e3abc238db2c4c4935ce206cf7df4eca089d95ed
Rafael Valentim <rmoraesvalentim@gmail.com>
2018-03-12 20:02:53 -0300

    Insercao do vendor com a classe SQL

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit7684e7f2bd97ec3caebf556c46f97948
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit7684e7f2bd97ec3caebf556c46f97948
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit7684e7f2bd97ec3caebf556c46f97948::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit7684e7f2bd97ec3caebf556c46f97948::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit7684e7f2bd97ec3caebf556c46f97948::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit7684e7f2bd97ec3caebf556c46f97948::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit7684e7f2bd97ec3caebf556c46f97948::$classMap;
 
